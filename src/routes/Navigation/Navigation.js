7ddb786335484d953c18bb1fedc62da929d0d6b6
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-03-31 17:22:39 +0300

    routing is done

---
+++ src/routes/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as Crown } from "../../assets/crown.svg";
+import "./Navigation.scss";
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Crown className="log" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
